765ad5a16f631a1640ddbf80003abf366f5f4ece
alphacpc <alphacpc@gmail.com>
2021-08-02 21:46:48 +0000

    Added Maps and initial About

--- ui/src/Pages/About.js
+++ ui/src/Pages/About.js
@@ -1,16 +1,86 @@
-import { Container } from "@material-ui/core";
-import Breadcrumb from "../components/Breadcrumb";
+import { Container, Grid, Link, Typography } from "@material-ui/core";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
+import Banniere from "./../assets/images/ban-contact.png";
+
+import PhoneIphoneOutlinedIcon from '@material-ui/icons/PhoneIphoneOutlined';
+import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 
 export default function About() {
+
+    const text = `Lorem Ipsum is simply dummy text of the printing 
+    and typesetting industry. Lorem Ipsum has been the industry's 
+    standard dummy text ever since the 1500s, when an unknown printer
+    took a galley of type and scrambled it to make a type specimen 
+    book. It has survived not only five centuries, but also the leap 
+    into electronic typesetting, remaining essentially unchanged. 
+    It was popularised in the 1960s with the release of Letraset 
+    sheets containing Lorem Ipsum passages, and more recently with 
+    desktop publishing software like Aldus PageMaker including 
+    versions of Lorem Ipsum. It is a long established fact that a 
+    reader will be distracted by the readable content of a page when 
+    looking at its layout. The point of using Lorem Ipsum is that it 
+    has a more-or-less normal distribution of letters, as opposed to 
+    using 'Content here, content here', making it look like readable 
+    English. Many desktop publishing packages and web page editors 
+    now use Lorem Ipsum as their default model text, and a search 
+    for 'lorem ipsum' will uncover many web sites still in their 
+    infancy. Various versions have evolved over the years, sometimes 
+    by accident, sometimes on purpose (injected humour and the like).
+    Lorem Ipsum is simply dummy text of the printing 
+    and typesetting industry. Lorem Ipsum has been the industry's 
+    standard dummy text ever since the 1500s, when an unknown printer
+    took a galley of type and scrambled it to make a type specimen 
+    book. It has survived not only five centuries, but also the leap 
+    into electronic typesetting, remaining essentially unchanged. 
+    It was popularised in the 1960s with the release of Letraset 
+    sheets containing Lorem Ipsum passages, and more recently with 
+    desktop publishing software like Aldus PageMaker including 
+    versions of Lorem Ipsum. It is a long established fact that a 
+    reader will be distracted by the readable content of a page when 
+    looking at its layout. The point of using Lorem Ipsum is that it 
+    has a more-or-less normal distribution of letters, as opposed to 
+    using 'Content here, content here', making it look like readable 
+    English. Many desktop publishing packages and web page editors 
+    now use Lorem Ipsum as their default model text, and a search 
+    for 'lorem ipsum' will uncover many web sites still in their 
+    infancy. Various versions have evolved over the years, sometimes 
+    by accident, sometimes on purpose (injected humour and the like).`
+
     return (
         <div className="AboutContainer">
             <Navbar/>
            
+           <div style={{background:`url(${Banniere}) no-repeat`, height:540}}/>
+
             <Container>
-                <Breadcrumb />
+                <div style={{marginTop:-80, backgroundColor:'#ffffff', padding: 20}}>
+                    <Typography variant="h3" component="h3">A propos de nous</Typography>
+                    <Typography variant="p" component="span">{text}</Typography>
+                    <Link>Ecrivez-nous</Link>
+                    <div>
+                        <Typography variant="h4" component="h4">Nos coordonnees</Typography>
+
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} md={4}>
+                                <Typography>Numero de telephone</Typography>
+                                <PhoneIphoneOutlinedIcon/>
+                            </Grid>
+                            <Grid Typography xs={12} md={4}>
+                                <Typography>Adresse email</Typography>
+                                <MailOutlineOutlinedIcon/>
+                            </Grid>
+                            <Grid Typography xs={12} md={4}>
+                                <Typography>Siege</Typography>
+                                {/* <AddLocationAltOutlinedIcon/> */}
+                                <LocationOnOutlinedIcon/>
+                            </Grid>
+                        </Grid>
+
+                    </div>
+                </div>                
             </Container>
 
             <Footer/>
--- ui/src/Pages/Contact.js
+++ ui/src/Pages/Contact.js
@@ -1,21 +1,33 @@
 import React, {useState} from 'react';
-import { Container } from "@material-ui/core";
+import { Container, Typography, Card, CardContent, Grid, TextField,Button } from "@material-ui/core";
 import Breadcrumb from "../components/Breadcrumb";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
-import KEY_API  from "./../Secret";
-import googleMapReact from 'google-map-react';
-import GoogleMap from 'google-map-react';
+import MapStyles from "./../MapStyle";
 
+import KEY_API  from "./../Secret";
+
+import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
+
+
+function Map(){
+    return(
+        <GoogleMap
+        defaultZoom={14}
+        defaultCenter={{ lat: 14.716677, lng: -17.467686 }}
+        defaultOptions={{styles: MapStyles}}
+        >
+            <Marker
+                position={{ lat: 14.716677, lng: 14.716677 }}
+            />
+        </GoogleMap>
+    )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map))
 
 export default function Contact() {
-
-    const center = useState({ lat: 59.95, lng: 30.33});
-    // const zoom = useState(11);
-    const zoom = 10
-
-    const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
     return (
         <div className="ContactContainer">
@@ -25,19 +37,47 @@ export default function Contact() {
             {/* <Container>
                 <Breadcrumb />
             </Container> */}
-            <div style={{ height: '60vh', width: '100%' }}>
-                <GoogleMap
-                bootstrapURLKeys={{ key: KEY_API }}
-                defaultCenter={center}
-                defaultZoom={zoom}
-                >
-                <AnyReactComponent
-                    lat={59.955413}
-                    lng={30.337844}
-                    text="My Marker"
+            <div style={{ height: '80vh', width: '100%' }}>
+                <WrappedMap 
+                    googleMapURL = {`https://maps.googleapis.com/maps/api/js?key=${KEY_API}&v=3.exp&libraries=geometry,drawing,places`}
+                    loadingElement = {<div style={{ height: `100%` }} />}
+                    containerElement = {<div style={{ height: `600px` }} />}
+                    mapElement = {<div style={{ height: `100%` }} />}
                 />
-                </GoogleMap>
             </div>
+            
+
+            <Container align="center">
+
+                <div style={{maxWidth: '80%', backgroundColor:'#FFF', marginTop:40, boxShadow:'8px 8px 8px 0 #000',
+            paddingTop: 40}}>
+                    <Typography variant="h3" align="center" style={{textTransform:'uppercase'}}>Contactez-nous</Typography>
+
+                    <CardContent>
+                        <form>
+                        <Grid xs={12} sm={6} style={{margin:'20px 0'}} item>
+                            <TextField label="Prenom" placeholder="Entrer votre prenom" variant="outlined" fullWidth required/>
+                        </Grid>
+
+                        <Grid xs={12} sm={6} style={{margin:'20px 0'}} item>
+                            <TextField label="Nom de famille" placeholder="Entrer votre nom de famille" variant="outlined" fullWidth required/>
+                        </Grid>
+
+                        <Grid xs={12} sm={12} style={{margin:'20px 0'}} item>
+                            <TextField label="Adresse e-mail" placeholder="Entrer votre adresse e-mail" variant="outlined" fullWidth required/>
+                        </Grid>
+
+                        <Grid xs={12} sm={12} style={{margin:'20px 0'}} item>
+                            <TextField label="Votre message" multiline rows={5} placeholder="Ecrivez votre message ..." variant="outlined" fullWidth required/>
+                        </Grid>
+
+                        <Grid xs={12} sm={4} item>
+                            <Button type="submit" variant="contained" color="secondary">Envoyez</Button>
+                        </Grid>
+                        </form>
+                    </CardContent>
+                </div>
+            </Container>
 
             <Footer />
 
